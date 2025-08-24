ac116b473eb0a686fc333d982818e534bb1e079c
karuna <karunaissa400@gmail.com>
2025-08-24 13:31:02 +0000

    Set up basic project structure

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from app import db
+
+class Incident(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
